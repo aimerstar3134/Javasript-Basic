2f81b2c188616bbbe9d7dc238f24c1fc7cf377a6
Harshil Patel <harshilp3134@gmail.com>
2026-01-11 23:45:02 +0530

    array methods

--- Array.js
+++ Array.js
@@ -2,3 +2,39 @@ let arr = [10, 20, 30, 40];
 
 console.log(arr[0]);
 console.log(arr.length);
+
+// arr.push(50);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(1);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.indexOf(40)); 
+
+//console.log(arr.includes(20));
+
+// let newArr = arr.slice(1, 3);
+// console.log(newArr);
+
+// arr.splice(1, 2, 25, 35);
+// console.log(arr);
+
+// arr.reverse();
+// console.log(arr);
+
+//arr.forEach(num => console.log(num * 2));
+
+// let result = arr.map(num => num * 2);
+// console.log(result);
+
+// let even = arr.filter(num => num % 2 === 0);
+// console.log(even);
+
+// let sum = arr.reduce((total, num) => total + num, 0);
+// console.log(sum);
