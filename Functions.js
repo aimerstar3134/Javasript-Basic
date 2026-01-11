73d154126fb933ac7ae210446c050ea6a7a3c233
Harshil Patel <harshilp3134@gmail.com>
2026-01-11 23:50:07 +0530

    Function practice

--- Functions.js
+++ Functions.js
@@ -6,3 +6,38 @@ console.log(add(5, 3));
 
 const multiply = (a, b) => a * b;
 console.log(multiply(4, 5));
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+console.log(subtract(10, 4));
+
+function greet(name, callback) {
+    console.log("Hello " + name);
+    callback();
+}
+
+function sayBye() {
+    console.log("Goodbye");
+}
+
+greet("Harshil", sayBye);
+
+
+function multiplier(factor) {
+    return function(num) {
+        return num * factor;
+    };
+}
+
+let double = multiplier(2);
+console.log(double(5));
+
+
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
