ce4a935550b3010caf60608a9a34f633681394c7
Harshil <harshil.p@sarvadhi.com>
2026-01-14 11:01:25 +0530

    crud with express using http server and mongodb

---
+++ express/src/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import userController from '../controller/user.controller.js';
+
+const router = express.Router();
+
+router.post('/users', userController.createUser);
+router.get('/users', userController.getUsers);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
+
+export default router;
